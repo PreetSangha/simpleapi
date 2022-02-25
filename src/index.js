78d107d6e6becac9940488fcb1b956a535acf129
Preet X Sangha <Preet.Sangha@serko.com>
2022-02-26 11:35:02 +1300

    add dotenv to manage environment variables

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+
+const dotenv = require('dotenv');
+dotenv.config(); 
+
 const express = require("express");
 
 const app = express();
 
-const port = 3000;
 
 app.get("/", (request, response) => {
     response
     .send('Home Page - default text');
 });
 
+const port = process.env.SERVER_PORT;
 app.listen(port, () => {
-    console.log(`express server listening on port ${port} `)
+    console.log(`express server listening on port ${port} `);
 });
