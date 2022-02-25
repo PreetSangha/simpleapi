825f6ce9191aef6a60b00777593635d356a0ed82
Preet X Sangha <Preet.Sangha@serko.com>
2022-02-26 10:46:07 +1300

    add / route and nodemon for local dev

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 
 const port = 3000;
 
+app.get("/", (request, response) => {
+    response
+    .json('Home Page - default text');
+});
+
 app.listen(port, () => {
     console.log(`express server listening on port ${port} `)
 });
