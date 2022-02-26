3d73b02e8b695b822762e555a1053cf53514edea
Preet X Sangha <Preet.Sangha@serko.com>
2022-02-26 20:53:49 +1300

    add better secrets management using doppler

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 First app using express.js
 
+you need to set the `SERVER_PORT` environment variable with the port that this server will run on, either directly, using something like doppler or an .env file. If you're using doppler and also want an direct override use
+
+```
+doppler run --preserve-env ...
+```
+and the approppriate start command.
+
 it can be started in dev with `npm start` and should output 
 
 ```
@@ -7,6 +14,7 @@ it can be started in dev with `npm start` and should output
 [nodemon] starting `node src/index.js`
 express server listening on port 3000
 ```
+
 
 or in prod with `node .\src\index.js` and should output
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+// see https://stackoverflow.com/questions/70309135/chalk-error-err-require-esm-require-of-es-module
+// for details of why version 5 of chalk need more setup than this require
+const chalk = require("chalk");
 
 const dotenv = require('dotenv');
 dotenv.config(); 
 
 const express = require("express");
-
 const app = express();
 
 
@@ -12,7 +14,17 @@ app.get("/", (request, response) => {
     .send('Home Page - default text');
 });
 
+
+function error (message) {
+    console.log(chalk.red(message));
+}
+
 const port = process.env.SERVER_PORT;
+
+if(port === undefined || port === null) {
+    error("SERVER_PORT environment variable not found");
+}
+
 app.listen(port, () => {
-    console.log(`express server listening on port ${port} `);
+    console.log(`express server listening on port http://localhost:${port} `);
 });
